e52ce1c4c9f1440a594496984a501dbc6a64126d
Brian Lois <Brian.Lois@capitalone.com>
2017-01-14 14:00:50 -0600

    added HW1

--- Week1/ggplot_examples.R
+++ Week1/ggplot_examples.R
@@ -1,10 +1,24 @@
 pokemon <- read.csv('/Users/lvi819/Documents/MIS6357/Week1/pokemon.csv')
 nrow(pokemon)
 ncol(pokemon)
-
+colnames(pokemon)
 
 ggplot(data = pokemon) +
   geom_point(mapping = aes(x = Attack, y = Defense))
 
 ggplot(data = pokemon) +
   geom_point(mapping = aes(x = Attack, y = Defense, color = Type_1))
+
+ggplot(data = pokemon) +
+  geom_point(mapping = aes(x = Attack, y = Defense, size = Weight_kg))
+
+ggplot(data = pokemon) +
+  geom_jitter(mapping = aes(x = Attack, y = Defense, size = Weight_kg))
+
+
+
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy )) +
+  geom_abline(aes(intercept=30, slope = -5)) +
+  geom_vline(aes(xintercept=5))
