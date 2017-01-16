b13d913e89c177832ba2ee18620f35e09de386b5
Brian Lois <Brian.Lois@capitalone.com>
2017-01-15 21:03:17 -0600

    added PCA

--- r_examples.R
+++ r_examples.R
@@ -16,5 +16,15 @@ bc <- BoxCoxTrans(x)
 x_bc <- predict(bc, x)
 ggplot() + aes(x_bc) + geom_histogram(binwidth = .1)
 
+#PCA
+x_1 <- c(0,1,2,3,4,5)
+x_2 <- c(.01,1.05,1.97,3.2,4.1,4.8)
+data <- cbind(x_1, x_2)
+plot_1 <- ggplot(data = as.data.frame(data)) + geom_point(mapping=aes(x=x_1, y = x_2))
+PCA_obj <- prcomp(data, center = FALSE, scale = FALSE)
+attributes(PCA_obj)
+PCA_obj$x %*% t(PCA_obj$rotation)
+plot_1 + geom_point(data = as.data.frame(PCA_obj$x), mapping = aes(x = PC1, y = PC2), color = 'red')
+
 
 
